cfe676e4db05f1f5eb4f230aac9990701446ee8e
Nikos Korfiatis <nikos.korfiatis@gmail.com>
2023-03-09 23:06:17 +0000

    Delete regression_plot.R

--- regression_plot.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-ggplot(mtcars, aes(x = wt, y = mpg)) + 
-  geom_point() +
-  geom_smooth(method = lm, se = FALSE) +
-  labs(title = "Regression Plot of Weight and Miles per Gallon",
-       x = "Weight (1000 lbs)",
-       y = "Miles per Gallon")
-ggsave("regression_plot.png")
