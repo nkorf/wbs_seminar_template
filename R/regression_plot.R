b3146ad2f2f655cc188399c2e7350f89de1c1761
Nikolaos Korfiatis <nikos.korfiatis@gmail.com>
2023-03-09 23:20:57 +0000

    changed the

--- R/regression_plot.R
+++ R/regression_plot.R
@@ -5,4 +5,10 @@ ggplot(mtcars, aes(x = wt, y = mpg)) +
   labs(title = "Regression Plot of Weight and Miles per Gallon",
        x = "Weight (1000 lbs)",
        y = "Miles per Gallon")
-ggsave("figures/regression_plot.png")
+
+this_filename_date <- as.character(Sys.Date())
+# format the Sys.time() to show only hours and minutes 
+this_filename_time <- as.character(format(Sys.time(), format = "%H_%M"))
+ggsave(paste0("figures/regression_plot_",
+              this_filename_date,"_",
+              this_filename_time,".png"))
